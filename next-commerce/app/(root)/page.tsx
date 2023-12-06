ff8c9057a6337c684932e0155a736d340bf52450
Damilola john <damilolajohn622@gmail.com>
2023-12-06 18:00:12 +0100

    Adding components

---
+++ next-commerce/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="row p-4">
+      <Button>Click Me</Button>
+    </div>
+    )
+}
