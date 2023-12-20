a38c77a65fae402407c5b698addeebfdac21613b
Damilola john <damilolajohn622@gmail.com>
2023-12-20 08:39:10 +0100

    added the store for testing products

--- next-commerce/app/layout.tsx
+++ next-commerce/app/layout.tsx
@@ -1,28 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToastProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Next-Commerce',
-  description: 'Revolutionizing E-commerce with Next.js',
+export const metadata = {
+  title: 'Dashboard',
+  description: 'E-Commerce Dashboard',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ModalProvider />
-          {children}
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="system" 
+            enableSystem
+          >
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
